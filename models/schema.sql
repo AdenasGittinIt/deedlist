b5d90c63b2e13647b69ebdb4d4ae4a8155fa6d7a
carinalullman <carinalullman@gmail.com>
2019-12-19 14:58:29 -0700

    Schema created
    This was based on our whiteboarding

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS deedlist_db;
+CREATE DATABASE deedlist_db;
+USE deedlist_db;
+
+CREATE TABLE people (
+  id int AUTO_INCREMENT NOT NULL,
+  name VARCHAR (30) NOT NULL,
+  zipcode INT (30) NOT NULL,
+  cellNumber INT (10) NOT NULL,
+  PRIMARY KEY(id)
+);
+CREATE TABLE need (
+  id int AUTO_INCREMENT NOT NULL,
+  title varchar(30) NOT NULL,
+  category VARCHAR(30) NOT NULL,
+  details VARCHAR (30) NOT NULL,
+  status VARCHAR (30) NOT NULL,
+  PRIMARY KEY(id)
+);
